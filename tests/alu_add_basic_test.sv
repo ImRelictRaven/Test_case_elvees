8801590068cf070996682c7f68951c424b674b4f
Relict Raven <relictraven@kaliRaven.Relictraven>
2025-11-24 23:07:15 +0300

    minor_fixes

--- tests/alu_add_basic_test.sv
+++ tests/alu_add_basic_test.sv
@@ -4,7 +4,9 @@ task automatic test_add_basic(output bit success);
 
   int          i;
   int unsigned num_tests;
+  logic [16:0] sum_ext;
   logic [15:0] expected;
+  logic        expected_cout;
   logic [15:0] A_log;
   logic [15:0] B_log;
   logic [15:0] logic_result;
@@ -21,29 +23,31 @@ task automatic test_add_basic(output bit success);
 
   for (i = 0; i < num_tests; i++) begin
 
-    // берём случайные 16 младших бит из $urandom(), чтобы избежать предупреждений по ширине
     A_log = $urandom()[15:0];
     B_log = $urandom()[15:0];
 
-    expected = A_log + B_log;
+    sum_ext       = {1'b0, A_log} + {1'b0, B_log};
+    expected      = sum_ext[15:0];
+    expected_cout = ~sum_ext[16];
 
     alu_intf.a = A_log;
     alu_intf.b = B_log;
 
     #10;
     logic_result = alu_intf.result;
-    //вместо assert стоит if чтобы тест не падал
-    if (logic_result !== expected) begin
+
+    if (logic_result !== expected || alu_intf.Cout !== expected_cout) begin
       $display(
-        "[%0t] [test_add_basic] ADD FAILED: A_log=%h B_log=%h Cin=%b logic_result=%h expected=%h",
-        $time, A_log, B_log, alu_intf.Cin, logic_result, expected
+        "[%0t] [test_add_basic] ADD FAILED: A=%h B=%h Cin=%b -> result=%h (exp=%h)  Cout=%b (exp=%b)",
+        $time, A_log, B_log, alu_intf.Cin,
+        logic_result, expected, alu_intf.Cout, expected_cout
       );
       success = 1'b0;
     end
     else begin
       $display(
-        "[%0t] [test_add_basic] ADD OK: A_log=%h B_log=%h Cin=%b result=%h",
-        $time, A_log, B_log, alu_intf.Cin, logic_result
+        "[%0t] [test_add_basic] ADD OK: A=%h B=%h Cin=%b result=%h Cout=%b",
+        $time, A_log, B_log, alu_intf.Cin, logic_result, alu_intf.Cout
       );
     end
   end
